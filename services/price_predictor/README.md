86e34aae8a14c46432984d58efab4725a679f991
gil612 <sgba000@gmail.com>
2025-02-04 21:34:02 +0100

    Initialize price predictor service with Hopsworks feature store integration
    - Add configuration management for feature view and data reading
    - Implement OhlcDataReader for reading OHLC data from online and offline feature stores
    - Create initial training script structure for model development
    - Set up project dependencies and development environment with Poetry

---
+++ services/price_predictor/README.md
@@ -0,0 +1,7 @@
+In price predictor we are going to train the pipeline that will be used to predict the price of the stock.
+
+The pipeline will be trained using the feature view that we created in the feature_store service.
+
+The pipeline will be trained using the training data that we have in the feature store.
+
+
---
+++ services/price_predictor/src/config.py
@@ -0,0 +1,29 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class AppConfig(BaseSettings):
+    feature_view_name: str = "ohlcv_feature_view"
+    feature_view_version: int = 1
+    feature_group_name: str 
+    feature_group_version: int
+    ohlc_window_sec: int
+    product_id: str
+    last_n_days: int
+
+
+
+    class Config:
+        env_file = ".env"
+
+class HopsworksConfig(BaseSettings):
+    hopsworks_project_name: str
+    hopsworks_api_key: str
+    class Config:
+        env_file = "hopsworks_credentials.env"
+    
+
+
+config = AppConfig()
+hopsworks_config = HopsworksConfig()
+
+
+
